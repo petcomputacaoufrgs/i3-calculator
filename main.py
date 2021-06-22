1ed2f39ce9b7cf50e90ee8ceb760ec74d48ec8fa
Victoria <victoria.ad40@gmail.com>
2021-06-22 15:20:52 -0300

    Feat (save options)

--- main.py
+++ main.py
@@ -1,4 +1,43 @@
 from bs4 import BeautifulSoup
+import os
+
+
+def has_extension(raw_file_name: str) -> bool:
+    """Check if the file name given has an extension
+    :param raw_file_name: file's name
+    :return: boolean indicating if the file has the extension or not
+    """
+    if len(raw_file_name.split(".")) == 1:
+        return False
+    return True
+
+
+def add_extension(raw_file_name: str, file_extension: str) -> str:
+    """Give the file extension to the file
+    :param raw_file_name: file's name
+    :param file_extension: extension of the file
+    :return: file with the '.csv' extension
+    """
+    return raw_file_name + file_extension
+
+
+def get_valid_file_name(message, extension, need_to_exist=True) -> str:
+    """ Get a valid file name (file name that exists in the computer)
+    :param message: question soliciting the specific file needed
+    :param extension: extension of the file
+    :return: valid file name
+    """
+    found = False
+    input_file = input(message)
+
+    while not found:
+        if not has_extension(input_file):
+            input_file = add_extension(input_file, extension)
+        if not need_to_exist or os.path.exists(input_file):
+            found = True
+        else:
+            input_file = input('Arquivo não encontrado. \nPor favor, tente novamente: ')
+    return input_file
 
 
 def calculate_grades(info_table):
@@ -39,11 +78,12 @@ def calculate_i3(absolute_grades):
 
 def save_info(student_info, destination_file):
     file = open(destination_file, "a")
-    file.write(f"""
-Candidato: {student_info.name}
+    file.write(f"""Candidato: {student_info.name}
 I3: {student_info.I3}
 I3 por semestre: 
-{student_info.grades_per_semester}""")
+{student_info.grades_per_semester}
+
+""")
     file.close()
 
 
@@ -67,5 +107,23 @@ class Student:
 
 
 if __name__ == '__main__':
-    student = Student("historico.html")
-    save_info(student, 'information.txt')
+    # Tem que ser o histórico do curso
+    over = False
+    input_file = get_valid_file_name(
+        'Insira o nome do arquivo .html do histórico do curso do aluno (com ou sem extensão): ', '.html')
+    student = Student(input_file)
+
+    save_file = get_valid_file_name(
+        'Insira o nome do arquivo .txt onde os dados obtidos serão salvos (com ou sem extensão): ', '.txt', False)
+
+    while not over:
+        save_info(student, save_file)
+        continue_process = input('Deseja continuar lendo arquivos? (y/n): ')
+        if continue_process == 'n':
+            over = True
+            continue
+        input_file = get_valid_file_name('Insira o novo arquivo .html a ser lido (com ou sem extensão): ', '.html')
+        use_same_file = input('Deseja salvar os dados no mesmo arquivo .txt de antes? (y/n): ')
+        if use_same_file == 'y':
+            continue
+        save_file = get_valid_file_name('Insira o nome do novo arquivo .txt (com ou sem extensão): ', '.txt', False)
